dfaee7bf635ba654e7fbb6e9220e7bda7b8ac770
uchesam <120647955+uchesam@users.noreply.github.com>
2026-02-21 03:18:30 +0100

    Add JobCard component for Maintify NG

---
+++ frontend/src/components/JobCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JobCard = () => {
+    return (
+        <div className="job-card">
+            <h3>Job Title</h3>
+            <p>Job Description goes here.</p>
+        </div>
+    );
+};
+
+export default JobCard;

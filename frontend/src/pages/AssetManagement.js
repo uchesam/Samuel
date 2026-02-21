88de90f0408520d72aaa6d7501ec02bb7f7cf514
uchesam <120647955+uchesam@users.noreply.github.com>
2026-02-21 03:19:41 +0100

    Create AssetManagement.js for asset management page

---
+++ frontend/src/pages/AssetManagement.js
@@ -0,0 +1,14 @@
+// AssetManagement.js
+
+import React from 'react';
+
+const AssetManagement = () => {
+    return (
+        <div>
+            <h1>Asset Management Page for Maintify NG</h1>
+            {/* Add your asset management components here */}
+        </div>
+    );
+};
+
+export default AssetManagement;

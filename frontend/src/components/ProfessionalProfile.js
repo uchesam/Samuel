1ff0d8afa8a31780e484ac274688c7c5b9923bba
uchesam <120647955+uchesam@users.noreply.github.com>
2026-02-21 03:18:22 +0100

    Add ProfessionalProfile component for Maintify NG

---
+++ frontend/src/components/ProfessionalProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProfessionalProfile() {
+  return (
+    <div>
+      <h1>Professional Profile</h1>
+      <p>Your professional profile details go here.</p>
+    </div>
+  );
+}
+
+export default ProfessionalProfile;
